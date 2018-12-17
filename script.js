4003a96b393d5b9ee03d1f1f7bd40921ecb1ffcc
snehithk <ksnehith@live.com>
2018-12-17 15:18:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var output = document.getElementById("output");
+
+function ud(n){ 
+output.innerHTML += n ;
+}
+
+function calculato() {
+output.innerHTML='';
+}
+
+document.getElementById("1").addEventListener("click",function(){ud(1);});
+document.getElementById("2").addEventListener("click",function(){ud(2);});
+document.getElementById("3").addEventListener("click",function(){ud(3);});
+document.getElementById("4").addEventListener("click",function(){ud(4);});
+document.getElementById("5").addEventListener("click",function(){ud(5);});
+document.getElementById("6").addEventListener("click",function(){ud(6);});
+document.getElementById("7").addEventListener("click",function(){ud(7);});
+document.getElementById("8").addEventListener("click",function(){ud(8);});
+document.getElementById("9").addEventListener("click",function(){ud(9);});
+document.getElementById("1").addEventListener("click",function(){ud(1);});
+document.getElementById("1").addEventListener("click",function(){ud(1);});
+document.getElementById("1").addEventListener("click",function(){ud(1);});
